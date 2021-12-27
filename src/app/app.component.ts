dd034ac0617063d37f8e7ef22d6f182e24868abf
itxahmedali <ahmedakhter1289@gmail.com>
2021-12-27 21:35:24 +0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter } from 'rxjs/operators';
+// import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'leaperway';
+  // pageName = [
+  //   'tabs',
+  //   'login',
+  // ]
+  // pageNameCon: boolean = false;
+
+  // public href: string = "";
+  constructor(
+    // private router: Router,
+    // private activatedRoute: ActivatedRoute,
+    // private titleService: Title
+  ) {
+  }
+
+
+  ngOnInit() {
+    // this.href = this.router.url;
+    // console.log(this.router.url);
+    // this.urlCheck(this.href);
+
+    // this.router.events.pipe(
+    //   filter(event => event instanceof NavigationEnd),
+    // ).subscribe(() => {
+    //   const rt = this.getChild(this.activatedRoute);
+    //   rt.data.subscribe(data => {
+    //     console.log(data);
+    //     this.titleService.setTitle(data.title)
+    //   });
+    // });
+  }
+
+  // getChild(activatedRoute: ActivatedRoute) {
+  //   if (activatedRoute.firstChild) {
+  //     return this.getChild(activatedRoute.firstChild);
+  //   } else {
+  //     return activatedRoute;
+  //   }
+
+  // }
+  // urlCheck(r) {
+  //   let url = r.split("/")[1]
+  //   if (url.includes(this.pageName)) {
+  //     this.pageNameCon = true
+  //   }
+  // }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+// import { ObservableService } from 'src/app/services/observable.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  // public href: string = "";
+  // cUrl:string = '';
+  // pageName = [
+  //   'tabs',
+  //   'login',
+  // ]
+  // pageNameCon: boolean = false;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(
+    // private router: Router,
+    // private cd: ChangeDetectorRef,
+    ) { }
+
+  ngOnInit() {
+    // this.href = this.router.url;
+    // ObservableService.webUrl.subscribe(value => {
+    //   console.log(this.href)
+    //   this.cUrl = value
+    //   this.urlCheck(value)
+    // })
   }
+
+  // urlCheck(r) {
+  //   console.log(r)
+  //   let url = r.split("/")[1]
+
+  //   for(var i = 0; i < this.pageName.length; i++){
+  //     if(this.pageName[i] == url){
+  //       this.pageNameCon = true;
+  //       break
+  //     }else{
+  //       this.pageNameCon = false;
+  //     }
+  //   }
+  //   this.cd.detectChanges();
+  // }
 
 }
